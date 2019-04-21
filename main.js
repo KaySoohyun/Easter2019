3a9e667d0f6d1656eda5b261884eaf9cab999bcc
Julieta Wiesztort <julieta.raw@gmail.com>
2019-04-21 13:10:50 -0300

    Added function reset() for refresh page

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 
+var pistas = [
+  {goodCode: "Poteto", newPlace: "Perro"}, 
+  {goodCode: "Toshi", newPlace: "Gato"}, 
+  {goodCode: "Luci", newPlace: "Gato"}
+]
 
-let i:number = 10;
+var i = -1;
+
+function reset() {
+  i = i + 1;
+  document.getElementById('saludo').style.display = 'none';
+  document.getElementById('app').style.display = 'block';
+  document.getElementById('nPista').innerHTML = "Pista " + (i + 1);
+  document.getElementById('place').innerHTML = pistas[i].newPlace;
+  document.getElementById('nextCode').style.display = 'none';
+  document.form.pista.value = '';
+  if(i == (pistas.length - 1)) {
+    document.getElementById('next').style.display = 'none';
+  }
+}
+
+function CheckPassword(inputtxt) {
+  if(inputtxt.value == pistas[i].goodCode) { 
+    document.getElementById('nextCode').style.display = 'block';
+  } else {
+    document.getElementById('tryAgain').style.display = 'block';
+  }
+}
+
